43803ea4361c78fa10edd2c7d96b716e527fd8af
Feepro <9822736q>
2021-10-20 07:09:15 +0300

    1

--- app/src/main/java/com/chopchop/minecloud/MainActivity.kt
+++ app/src/main/java/com/chopchop/minecloud/MainActivity.kt
@@ -1,59 +1,83 @@
 package com.chopchop.minecloud
 
-import android.app.UiModeManager.MODE_NIGHT_NO
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.util.Log
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
-import androidx.annotation.RequiresApi
+import android.widget.*
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.setDefaultNightMode
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.chopchop.minecloud.Utils.addMySubs
+import com.chopchop.minecloud.Utils.addSub
+import com.chopchop.minecloud.Utils.getBalance
+import com.chopchop.minecloud.Utils.getMySubs
+import com.chopchop.minecloud.Utils.getRazgon
+import com.chopchop.minecloud.Utils.getUserId
+import com.chopchop.minecloud.Utils.saveBalance
+import com.chopchop.minecloud.Utils.saveRazgon
+import com.chopchop.minecloud.Utils.saveUserId
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import com.google.android.material.bottomsheet.BottomSheetBehavior
-import kotlinx.coroutines.CommonPool
+import com.google.firebase.database.*
 import kotlinx.coroutines.android.UI
-import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.DriverManager.getConnection
-import java.sql.SQLException
+import java.math.RoundingMode
+import java.text.DecimalFormat
 import java.util.*
-import kotlin.random.Random
+
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var speedBottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var enterCodeBottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var myRef:DatabaseReference
+    var schet:Double = 0.00000001
+    var razgon = 1f;
+    var mRewardedAd: RewardedAd? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
-        bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet))
+        speedBottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet))
+        enterCodeBottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.enterCodeSheet))
         //bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+        val database = FirebaseDatabase.getInstance()
+        myRef = database.getReference("users_refers")
+
 
         val ballanceTW = findViewById<TextView>(R.id.balanceTW)
         val rubBalance = findViewById<TextView>(R.id.rubBalance)
         val speedTW = findViewById<TextView>(R.id.speedTW)
         val hashTW = findViewById<TextView>(R.id.hashTW)
         val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        var schet:Double = 0.00000000
-        var razgon = 1;
 
-        start()
+        schet = getBalance(this)
+        razgon = getRazgon(this)
+        updateRazgon()
+
+        createUser(ballanceTW,rubBalance)
+
+
+
+
 
 
         launch(UI) {
 
                 while (true){
-                schet+=0.00000001;
+                    saveBalance(this@MainActivity,schet)
+                    schet+=0.00000001;
                     progressBar.progress = 0
                     while(progressBar.progress < 99){
                         progressBar.progress +=10
-                        delay((5000..7000).random().toLong()/10/razgon)
+                        delay(((5000..7000).random()/10/razgon).toLong())
                         speedTW.text = "116"+ (0..99).random()+"."+(1000..9999).random()+" H/s"
 
                         val source = "abcdefghijklmnopqrstuvwxyz123456789123456789123456789"
@@ -65,38 +89,130 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     val strSchet = schet.toBigDecimal().toPlainString().subSequence(0,10).toString()
-                    ballanceTW.text = "$strSchet BTC"
-                    rubBalance.text = "~ "+(schet/0.000000371).toBigDecimal().toPlainString().subSequence(0,5).toString() + " ₽"
+                    ballanceTW.text = "$strSchet "
+                    rubBalance.text = "~ "+(schet/0.000000371).toBigDecimal().toPlainString().subSequence(0,5).toString() + " "
 
             }
         }
+        //init ADD
+        initAdMob()
+
     }
+
+    private fun initAdMob() {
+        MobileAds.initialize(this) {}
+        findViewById<AdView>(R.id.adView).loadAd(AdRequest.Builder().build())
+        var adRequest = AdRequest.Builder().build()
+        RewardedAd.load(this,"ca-app-pub-3940256099942544/5224354917", adRequest, object : RewardedAdLoadCallback() {
+            override fun onAdFailedToLoad(adError: LoadAdError) {
+                Log.d("TAG", adError?.message)
+                mRewardedAd = null
+            }
+
+            override fun onAdLoaded(rewardedAd: RewardedAd) {
+                Log.d("TAG", "Ad was loaded.")
+                mRewardedAd = rewardedAd
+            }
+        })
+
+
+    }
+
     fun onClickBoost(v: View){
-        bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-    }
-
-    internal var conn: Connection? = null
-
-
-
-    fun start(){
-        getConnection()
-    }
-    fun getConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance()
-            conn = DriverManager.getConnection("jdbc:mysql://cloud-mine-rus.zzz.com.ua:3306/cloudmine", "cloudmine", "ceepercom123COM")
-
-        } catch (ex: SQLException) {
-            // handle any errors
-            ex.printStackTrace()
-        } catch (ex: Exception) {
-            // handle any errors
-            ex.printStackTrace()
-        }
+        speedBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
     }
 
     fun enterCode(view: android.view.View) {
+        enterCodeBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+    }
+
+    private fun createUser(ballanceTW: TextView, rubBalance: TextView) {
+        val userIdTW = findViewById<TextView>(R.id.userId)
+        if(getUserId(this) == -1) {
+            val userId = (100000..999999).random()
+            saveUserId(this,userId)
+            myRef.child(userId.toString()).setValue(0)
+            userIdTW.text = userId.toString()
+        }else{
+            val userId = getUserId(this)
+            myRef.child(userId.toString()).addValueEventListener(object :ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if(snapshot.value.toString().toInt() > getMySubs(this@MainActivity)){
+                        addMySubs(this@MainActivity)
+                        schet+= 0.00005
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {}
+            })
+            userIdTW.text = userId.toString()
+            ballanceTW.text = "${schet.toBigDecimal().toPlainString().subSequence(0,10)} BTC"
+            rubBalance.text = "~ "+(schet/0.000000371).toBigDecimal().toPlainString().subSequence(0,5).toString() + " ₽"
+
+        }
+    }
+
+    fun sendCode(view: android.view.View) {
+        val userId2 = findViewById<EditText>(R.id.userId2)
+        myRef.child(userId2.text.toString()).get().addOnSuccessListener {
+            Log.i("firebase", "Got value ${it.value}")
+            if(it.value != null)
+                if(addSub(this,userId2.text.toString().toInt())){
+                    myRef.child(userId2.text.toString()).setValue(it.value.toString().toInt()+1)
+                    Toast.makeText(this,"Вы подписались на пользователя ${userId2.text}", Toast.LENGTH_SHORT).show()
+                    enterCodeBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+                else
+                    Toast.makeText(this,"Вы уже подписались на этого пользователя", Toast.LENGTH_SHORT).show()
+            else
+                Toast.makeText(this,"Пользователь не найден", Toast.LENGTH_SHORT).show()
+
+        }.addOnFailureListener{
+            Toast.makeText(this,"Ошибка получения данных", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    fun updateRazgon(){
+        val df = DecimalFormat("#.##")
+        df.roundingMode = RoundingMode.CEILING
+        findViewById<TextView>(R.id.razgonTW).text = "Разгон: ${df.format(razgon)}X"
+        saveRazgon(this,razgon)
+    }
+
+    fun addRazgonByAd(view: android.view.View) {
+        val addRazgonByAd = findViewById<Button>(R.id.addRazgonByAd)
+        val addRazgonByAdText = addRazgonByAd.text.toString()
+        if (mRewardedAd != null) {
+            mRewardedAd?.show(this@MainActivity) {
+                razgon += 0.05f
+                updateRazgon()
+                speedBottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                addRazgonByAd.isEnabled = false
+                var tick = 15
+                object : CountDownTimer(15000,1000){
+                    override fun onTick(p0: Long) {
+                        addRazgonByAd.text = "$addRazgonByAdText ${tick}сек"
+                        tick--
+                    }
+                    override fun onFinish() {
+                        addRazgonByAd.text = addRazgonByAdText
+                        addRazgonByAd.isEnabled = true
+                    }
+                }.start()
+                var adRequest = AdRequest.Builder().build()
+                RewardedAd.load(this,"ca-app-pub-3940256099942544/5224354917", adRequest, object : RewardedAdLoadCallback() {
+                    override fun onAdFailedToLoad(adError: LoadAdError) {
+                        Log.d("TAG", adError?.message)
+                        mRewardedAd = null
+                    }
+
+                    override fun onAdLoaded(rewardedAd: RewardedAd) {
+                        Log.d("TAG", "Ad was loaded.")
+                        mRewardedAd = rewardedAd
+                    }
+                })
+            }
+        }
 
     }
 }
---
+++ app/src/main/java/com/chopchop/minecloud/Utils.kt
@@ -0,0 +1,165 @@
+package com.chopchop.minecloud
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Environment
+import java.io.File
+import java.io.InputStream
+import android.graphics.drawable.VectorDrawable
+
+import android.graphics.BitmapFactory
+
+import android.graphics.drawable.BitmapDrawable
+
+import androidx.core.content.ContextCompat
+
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.media.MediaMetadataRetriever
+import java.lang.IllegalArgumentException
+
+
+object Utils {
+    fun getVideoDuration(context:Context,path: String): Long {
+        val retriever =  MediaMetadataRetriever();
+        retriever.setDataSource(context, Uri.fromFile(File(path)));
+        val time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+        retriever.release()
+        return time!!.toLong();
+    }
+
+
+    val outputPath: String
+        get() {
+            val path = Environment.getExternalStorageDirectory()
+                .toString() + File.separator + "Recorder_app" + File.separator
+
+            val folder = File(path)
+            if (!folder.exists())
+                folder.mkdirs()
+
+            return path
+        }
+
+    fun dpFromPx(context: Context, px: Float): Float {
+        return px / context.resources.displayMetrics.density
+    }
+
+    fun pxFromDp(context: Context, dp: Float): Float {
+        return dp * context.resources.displayMetrics.density
+    }
+
+    fun InputStream.toFile(path: String) {
+        File(path).outputStream().use { this.copyTo(it) }
+    }
+
+    fun getConvertedFile(folder: String, fileName: String): File {
+        val f = File(folder)
+
+        if (!f.exists())
+            f.mkdirs()
+
+        return File(f.path + File.separator + fileName)
+    }
+
+    fun refreshGallery(path: String, context: Context) {
+
+        val file = File(path)
+        try {
+            val mediaScanIntent = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+            val contentUri = Uri.fromFile(file)
+            mediaScanIntent.data = contentUri
+            context.sendBroadcast(mediaScanIntent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+    }
+    fun getBitmap(vectorDrawable: VectorDrawable): Bitmap? {
+        val bitmap = Bitmap.createBitmap(
+            vectorDrawable.intrinsicWidth,
+            vectorDrawable.intrinsicHeight, Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight())
+        vectorDrawable.draw(canvas)
+        return bitmap
+    }
+
+     fun getBitmap(context: Context, drawableId: Int): Bitmap? {
+        val drawable = ContextCompat.getDrawable(context, drawableId)
+        return if (drawable is BitmapDrawable) {
+            BitmapFactory.decodeResource(context.resources, drawableId)
+        } else if (drawable is VectorDrawable) {
+            getBitmap(drawable)
+        } else {
+            throw IllegalArgumentException("unsupported drawable type")
+        }
+    }
+    fun saveBalance(context: Context, balance:Double ){
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        val prefsEditor: SharedPreferences.Editor = mPrefs.edit()
+        prefsEditor.putFloat("balance", balance.toFloat())
+        prefsEditor.apply()
+    }
+    fun getBalance(context: Context): Double {
+
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        return mPrefs.getFloat("balance", 0.00000001f).toDouble()
+    }
+
+    fun saveUserId(context: Context, userId:Int ){
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        val prefsEditor: SharedPreferences.Editor = mPrefs.edit()
+        prefsEditor.putInt("userId", userId)
+        prefsEditor.apply()
+    }
+    fun getUserId(context: Context): Int {
+
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        return mPrefs.getInt("userId", -1)
+    }
+
+    fun addSub(context: Context, userId:Int ): Boolean {
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        val prefsEditor: SharedPreferences.Editor = mPrefs.edit()
+        val subs = mPrefs.getStringSet("subs",null)
+        val newSubs : MutableList<String> = if(subs != null)
+             subs.toMutableList()
+        else
+            ArrayList()
+
+        return if(newSubs.contains(userId.toString()))
+            false
+        else{
+            newSubs.add(userId.toString())
+            prefsEditor.putStringSet("subs",newSubs.toSet())
+            prefsEditor.apply()
+            true
+        }
+    }
+    fun addMySubs(context: Context){
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        val prefsEditor: SharedPreferences.Editor = mPrefs.edit()
+        prefsEditor.putInt("mySubs", getMySubs(context)+1)
+        prefsEditor.apply()
+    }
+    fun getMySubs(context: Context): Int {
+
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        return mPrefs.getInt("mySubs", 0)
+    }
+    fun saveRazgon(context: Context, razgon: Float){
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        val prefsEditor: SharedPreferences.Editor = mPrefs.edit()
+        prefsEditor.putFloat("razgon", razgon)
+        prefsEditor.apply()
+    }
+    fun getRazgon(context: Context): Float {
+
+        val mPrefs: SharedPreferences = context.getSharedPreferences("SPData", Context.MODE_PRIVATE)
+        return mPrefs.getFloat("razgon", 1f)
+    }
+}
